b90070957aad6533f8944593d0c2a3ad04592ba6
Erre <kali@Fisso>
2024-08-04 11:16:05 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+import loadotenv
+pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3.1-8B")
+loadotenv.load_env(".env")
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
